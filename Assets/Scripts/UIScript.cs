8c367079682be5fdd0d5d9d81583e68ff7c76d28
NayrL <40843838+NayrL114@users.noreply.github.com>
2022-09-25 20:35:56 +1000

    Simple UI and updated power pallet animation

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -8,29 +8,59 @@ public class UIScript : MonoBehaviour
 
     public Text GhostStateText;
     public Text PacStateText;
-    public float timer;
 
+    private float ghostTimer = 0;
+    private float pacTimer = 0;
     private string[] ghostStateStrings = { "Walking Left", "Walking Right", "Walking Up", "Walking Down", "Scared", "Recovering", "Death" };
-    private string[] pacStatesStrings = {"Walking Up",
-        "Death Facing Up", "Walking Right", "Death Facing Right", "Walking Down", "Death Facing Down", "Walking Left", "Death Facing Left"};
-    private int ghostArrayIter;
-    private int pacArrayIter;
+    private string[] pacStatesStrings = {"Walking & Death Facing Up",
+        "Walking & Death Facing Right", "Walking & Death Facing Down", "Walking Left & Death Facing Left", };
+    private int ghostArrayIter = 0;
+    private int pacArrayIter = 0;
         
     // Start is called before the first frame update
     void Start()
     {
-        ghostArrayIter = 0;
-        pacArrayIter = 0;
+        GhostStateText.text = ghostStateStrings[ghostArrayIter];
+        PacStateText.text = pacStatesStrings[pacArrayIter];
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        ghostTimer += Time.deltaTime;
+        pacTimer += Time.deltaTime;
+        //Debug.Log(timer);
 
-        if (timer % 3 == 0)
+        if (ghostTimer >= 3)
         {
+            ghostArrayIter++;
+            GhostStateText.text = ghostStateStrings[ghostArrayIter];
+            ghostTimer = 0;
 
         }
+
+        if (pacTimer >= 5.08f)
+        {
+            pacArrayIter++;
+            PacStateText.text = pacStatesStrings[pacArrayIter];
+            pacTimer = 0;
+
+        }
+
+        if (ghostArrayIter >= 6) ghostArrayIter = -1;
+
+        if (pacArrayIter >= 3) pacArrayIter = -1;
+
+        /*
+        if (timer % 5 == 0)
+        {
+            pacArrayIter++;
+            PacStateText.text = pacStatesStrings[pacArrayIter];
+
+        }
+        */
+
     }
+       
 }
