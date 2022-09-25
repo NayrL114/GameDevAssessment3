aced2c423cb117a55859f5aafa358c1a0a44e835
NayrL <40843838+NayrL114@users.noreply.github.com>
2022-09-25 23:54:44 +1000

    Final reference update for 1.0 release

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -62,5 +62,27 @@ public class UIScript : MonoBehaviour
         */
 
     }
+
+    /* 
+     * I reckon this might be a nice place for some audio references :p
+     *
+     *  PacMove: 566303__crazybeatsinc__pop-sound-effect.wav - https://freesound.org/people/CrazyBeatsINC/sounds/566303/
+
+        EatPallet: 610026__cc0-sound__04-mt-trx-xt.wav - https://freesound.org/people/CC0_Sound/sounds/610026/
+
+        PacDeath: 207321__patricklieberkind__machine-shutdown.wav https://freesound.org/people/PatrickLieberkind/sounds/207321/
+
+        WallHit: 13946__adcbicycle__18.wav - https://freesound.org/people/adcbicycle/sounds/13946/
+
+        IntroMusic: 326553__shadydave__the-sonata-piano-loop.mp3 - https://freesound.org/people/ShadyDave/sounds/326553/
+
+        NormalGhostMusic: 623616__davejf__melody-loop-107-bpm.mp3 - https://freesound.org/people/DaveJf/sounds/623616/
+
+        ScaredMusic: 256218__zagi2__guitar-riff - https://freesound.org/people/zagi2/sounds/256218/
+
+        DeadMusic: 501079__zagi2__ambush.wav - https://freesound.org/people/zagi2/sounds/501079/
+
+    */
+
        
 }
