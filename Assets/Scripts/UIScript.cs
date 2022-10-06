90a8cfb2bdbe30cc10d51a8491bd91e89bec2b1e
NayrL <40843838+NayrL114@users.noreply.github.com>
2022-10-06 22:33:32 +1100

    preliminary in game ui canvas

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -6,9 +6,10 @@ using UnityEngine.UI;
 public class UIScript : MonoBehaviour
 {
 
-    public Text GhostStateText;
-    public Text PacStateText;
+    //public Text GhostStateText;
+    //public Text PacStateText;
 
+    /*
     private float ghostTimer = 0;
     private float pacTimer = 0;
     private string[] ghostStateStrings = { "Walking Left", "Walking Right", "Walking Up", "Walking Down", "Scared", "Recovering", "Death" };
@@ -16,18 +17,21 @@ public class UIScript : MonoBehaviour
         "Walking & Death Facing Right", "Walking & Death Facing Down", "Walking Left & Death Facing Left", };
     private int ghostArrayIter = 0;
     private int pacArrayIter = 0;
+    */
         
     // Start is called before the first frame update
     void Start()
     {
-        GhostStateText.text = ghostStateStrings[ghostArrayIter];
-        PacStateText.text = pacStatesStrings[pacArrayIter];
+        //GhostStateText.text = ghostStateStrings[ghostArrayIter];
+        //PacStateText.text = pacStatesStrings[pacArrayIter];
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        /*
         ghostTimer += Time.deltaTime;
         pacTimer += Time.deltaTime;
         //Debug.Log(timer);
